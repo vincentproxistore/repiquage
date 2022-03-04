392f489d06cd29e2f5879ec743a9688afbb7ac54
Vincent Descamps <vincent.descamps@proxistore.com>
2022-03-04 13:19:45 +0100

    init and read csv

---
+++ index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+const csv = require('csv-parser');
+
+var server = http.createServer(function(req, res) {
+  fs.createReadStream('test.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row['ACCOUNTNAME']);
+    console.log(row['CITY']);
+  })
+  .on('end', (e) => {
+    console.log('CSV file successfully processed');
+  });
+});
+server.listen(8000, function(req, res) {
+  console.log('server listening to localhost 8000');
+});
